1f039e2320a596827982810d14ac036074b0cda4
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-23 23:34:50 -0500

    stub up USGovernment component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import USGovernment from './USGovernment';
 
 function App() {
   return (
     <div>
+      <USGovernment />
     </div>
   );
 }
---
+++ src/USGovernment.jsx
@@ -0,0 +1,11 @@
+const USGovernment = (props) => {
+  return (
+    <>
+      <ExecutiveBranch />
+      <JudicialBranch />
+      <LegislativeBranch />
+    </>
+  )
+}
+
+export default USGovernment
