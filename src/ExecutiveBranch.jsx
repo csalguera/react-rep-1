6e0dd6b3c01e56725352b86248c7c27867d024c1
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-23 23:35:48 -0500

    stub up ExecutiveBranch component

---
+++ src/ExecutiveBranch.jsx
@@ -0,0 +1,11 @@
+const ExecutiveBranch = (props) => {
+  return (
+    <>
+      <h1>President</h1>
+      <h1>Vice President</h1>
+      <h1>The Cabinet</h1>
+    </>
+  )
+}
+
+export default ExecutiveBranch
